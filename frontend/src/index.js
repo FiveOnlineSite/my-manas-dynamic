4fcff8a2505940bb84adf5dfd1ab2f5bcee4ce32
reacthnrtechnology <reacthnrtechnology@gmail.com>
2025-06-25 14:54:10 +0530

    file deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,9 +10,9 @@ import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
      <HelmetProvider>
     <App />
     </HelmetProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
